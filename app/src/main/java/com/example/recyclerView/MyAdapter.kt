3cb306100b20d2cc3c9fe124ee3a0ad84ee0b4f3
loulou <yourdj.france@gmail.com>
2020-04-28 21:51:14 +0200

    display 2 textView and edit content in adapter / recycler View !

--- app/src/main/java/com/example/recyclerView/MyAdapter.kt
+++ app/src/main/java/com/example/recyclerView/MyAdapter.kt
@@ -9,23 +9,6 @@ import androidx.recyclerview.widget.RecyclerView
 class MyAdapter(private val myDataset: Array<ObjectBonPlan>) :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder.
-    // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(val textView: View) : RecyclerView.ViewHolder(textView) {
-        lateinit var tv : TextView
-        lateinit var  tv2 : TextView
-
-        fun MyViewHolder(itemView : View) {
-            super.itemView
-
-            tv = itemView.findViewById(R.id.my_text_view)
-            tv2 = itemView.findViewById(R.id.my_text_view2)
-
-        }
-    }
-
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -39,14 +22,29 @@ class MyAdapter(private val myDataset: Array<ObjectBonPlan>) :
         return MyViewHolder(view)
     }
 
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        var tv : TextView = view.findViewById(R.id.my_text_view)
+        var tv2 : TextView = view.findViewById(R.id.my_text_view2)
+    }
+
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
 
+        val obj = myDataset[position]
 
+        val link = obj.link
+        val end = obj.end
 
-        holder.textView.text = myDataset[position]
+        holder.tv.text = link
+        holder.tv2.text = end
+
+        // holder.textView.text = myDataset[position]
     }
 
     // Return the size of your dataset (invoked by the layout manager)

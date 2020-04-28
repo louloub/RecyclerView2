3e568dc60d80824b6d68b41325231c3b4a6dc665
loulou <yourdj.france@gmail.com>
2020-04-28 21:21:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/recyclerView/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.recyclerView
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : Activity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+
+        val myDataset = arrayOf(
+                "test 1",
+                "test 2",
+                "test 3",
+                "test 4",
+                "test 5",
+                "test 6",
+                "test 7",
+                "test 8",
+                "test 9",
+                "test 10"
+        )
+        viewAdapter = MyAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
+    }
+}
